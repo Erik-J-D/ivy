2d50133d4b8e0e65342b2f77f9805fd2575a321e
David Rusu <davidrusu.me@gmail.com>
2022-01-22 14:27:31 -0500

    refactor: use happend instead of place-at

--- ui/cli.rkt
+++ ui/cli.rkt
@@ -13,40 +13,38 @@
   (match c
     [(cursor row col) (cursor (max 0 (+ dr row)) (max 0 (+ dc col)))]))
 
-(define (render-line r cursor row line)
+(define (render-line cursor row line)
   (define line-len (min width (string-length line)))
 
   (define (render-line-with-cursor)
     (let* ([real-cur-pos (cursor-col cursor)]
            [cur-pos (min real-cur-pos line-len)]
-           [line-before-cursor (substring line 0 cur-pos)]
            [char-at-cursor (cond
                              [(= cur-pos line-len) #\space]
-                             [else (string-ref line cur-pos)])]
-           [line-after-cursor (substring line (min line-len (+ cur-pos 1)))])
+                             [else (string-ref line cur-pos)])])
       (let ([rendered
-             (place-at*
-              r
-              [row 0 (text line-before-cursor)]
-              [row cur-pos (bg 'white (fg 'black (char char-at-cursor)))]
-              [row (+ 1 cur-pos) (text line-after-cursor)])])
+             (happend
+              (text (substring line 0 cur-pos))
+              (bg 'white (fg 'black (char char-at-cursor)))
+              (text (substring line (min line-len (+ cur-pos 1)))))])
 
-        (if (not (= cur-pos real-cur-pos))
-            ;; If our cursor is beyond the end-of-line, render a cursor shadow
-            ;; where our pointer actually is.
-            (place-at rendered row real-cur-pos (bg 'brblack (char #\space)))
-            rendered))))
+        (cond
+          [(not (= cur-pos real-cur-pos))
+           ;; If our cursor is beyond the end-of-line, render a cursor shadow
+           ;; where our pointer actually is.
+           (place-at rendered 0 real-cur-pos (bg 'brblack (char #\space)))]
+          [else rendered]))))
 
   (cond
     [(= row (cursor-row cursor)) (render-line-with-cursor)]
-    [else (place-at r row 0 (text (substring line 0 line-len)))]))
+    [else (text (substring line 0 line-len))]))
 
 (define (render-buffer w initial-r)
   (match w
     [(ivy buf cursor)
      (let-values ([(r i)
                    (for/fold ([r initial-r] [row 0]) ([line (in-list buf)])
-                     (values (render-line r cursor row line) (+ row 1)))])
+                     (values (place-at r row 0 (render-line cursor row line)) (+ row 1)))])
        r)]))
 
 (struct ivy (buffer cursor)
